3b0d07a1ba7138bc71f924a7ccc09f25b1dbbbe4
Mateus Nogueira <msznogueira@gmail.com>
2025-05-21 10:50:15 -0300

    Using subreddit name and sort method when requesting submissions and saving the results.

--- thread.py
+++ thread.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 reddit = praw.Reddit("bot1", user_agent="bot1 user agent")
 
-subreddit = reddit.subreddit("netsec")
+subreddit_name = "deeplearning"
+subreddit = reddit.subreddit(subreddit_name)
+sort_by_method = "top"
 submission_rows = []
 comment_rows = []
 submission_row_keys = {"id", "title", "upvote_ratio", "score", "selftext", "poll_data", "over_18", "num_comments", "locked", "is_self", "author", "author_flair_text", "url", "link_flair_text"}
 comment_row_keys = {"id", "parent_id", "score", "body", "created_utc", "is_submitter", "author"}
-for submission in subreddit.hot(limit=10):    
+for submission in subreddit.__getattr__(sort_by_method)(limit=50, time_filter="all"):
     submission_row = {key:getattr(submission, key, None) for key in submission_row_keys}
     for comment in submission.comments:        
         comment_row = {key:getattr(comment, key, None) for key in comment_row_keys}
@@ -18,6 +20,6 @@ for submission in subreddit.hot(limit=10):
 df_submissions = pd.DataFrame(submission_rows)
 df_comments = pd.DataFrame(comment_rows)
 
-df_submissions.to_csv("submissions.csv", index=False)
-df_comments.to_csv("comments.csv", index=False)
+df_submissions.to_csv(f"{subreddit_name}_submissions_{sort_by_method}.csv", index=False)
+df_comments.to_csv(f"{subreddit_name}_comments_{sort_by_method}.csv", index=False)
 
